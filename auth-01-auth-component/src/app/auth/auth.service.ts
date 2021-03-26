7bb1b26cc84d26a84eaf17da73fdc8bbc7ef8c5d
John Fitzpatrick <john.fitzpatrick@sendflex.com>
2021-03-26 11:26:00 -0400

    User auth

--- auth-01-auth-component/src/app/auth/auth.service.ts
+++ auth-01-auth-component/src/app/auth/auth.service.ts
@@ -1,68 +1,110 @@
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { catchError } from 'rxjs/operators';
-import { throwError } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { catchError, tap } from "rxjs/operators";
+import { Subject, throwError } from "rxjs";
+import { User } from "./user.model";
 
 export interface AuthResponseData {
-  kind: string,
-  idToken: string,
-  email: string,
-  refreshToken: string,
-  expiresIn: string,
-  localId: string,
-  registered?: boolean
+  kind: string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered?: boolean;
 }
 
-@Injectable({ providedIn: 'root' })
+@Injectable({ providedIn: "root" })
 export class AuthService {
+  user = new Subject<User>();
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   signUp(email: string, password: string) {
-    return this.http.post<AuthResponseData>('', {
-      email: email,
-      password: password,
-      returnSecureToken: true
-    }).pipe(catchError(this.handleError));
+    return this.http
+      .post<AuthResponseData>(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCR-zDnEkd3aTs-19HHrBAZKXPbIqKHu9Y",
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }
+      )
+      .pipe(
+        catchError(this.handleError),
+        tap((resData) => {
+          this.handleAuthentication(
+            resData.email,
+            resData.localId,
+            resData.idToken,
+            +resData.expiresIn
+          );
+        })
+      );
   }
 
   login(email: string, password: string) {
-    return this.http.post<AuthResponseData>('', {
-      email: email,
-      password: password,
-      returnSecureToken: true
-    }).pipe(catchError(this.handleError));
+    return this.http
+      .post<AuthResponseData>(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCR-zDnEkd3aTs-19HHrBAZKXPbIqKHu9Y",
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }
+      )
+      .pipe(
+        catchError(this.handleError),
+        tap((resData) => {
+          this.handleAuthentication(
+            resData.email,
+            resData.localId,
+            resData.idToken,
+            +resData.expiresIn
+          );
+        })
+      );
+  }
+
+  private handleAuthentication(
+    email: string,
+    userId: string,
+    token: string,
+    expiresIn: number
+  ) {
+    const expirationDate = new Date(new Date().getTime() + +expiresIn * 1000);
+    const user = new User(email, userId, token, expirationDate);
+    this.user.next(user);
   }
 
   private handleError(errorRes: HttpErrorResponse) {
-    let errorMessage = 'An unknown error occured';
+    let errorMessage = "An unknown error occured";
 
-    console.log('errorRes ', errorRes);
+    console.log("errorRes ", errorRes);
 
     if (!errorRes.error || !errorRes.error.error) {
       return throwError(errorMessage);
     }
     switch (errorRes.error.error.message) {
-      case 'EMAIL_EXISTS':
-        errorMessage = 'This email exists already';
+      case "EMAIL_EXISTS":
+        errorMessage = "This email exists already";
         break;
-      case 'OPERATION_NOT_ALLOWED':
-        errorMessage = 'Sign-in disabled';
+      case "OPERATION_NOT_ALLOWED":
+        errorMessage = "Sign-in disabled";
         break;
-      case 'TOO_MANY_ATTEMPTS_TRY_LATER':
-        errorMessage = 'Too many attempts. Account locked';
+      case "TOO_MANY_ATTEMPTS_TRY_LATER":
+        errorMessage = "Too many attempts. Account locked";
         break;
-      case 'EMAIL_NOT_FOUND':
-        errorMessage = 'Account not found';
+      case "EMAIL_NOT_FOUND":
+        errorMessage = "Account not found";
         break;
-      case 'INVALID_PASSWORD':
-        errorMessage = 'Password is not correct';
+      case "INVALID_PASSWORD":
+        errorMessage = "Password is not correct";
         break;
-      case 'USER_DISABLED':
-        errorMessage = 'Account not available';
+      case "USER_DISABLED":
+        errorMessage = "Account not available";
         break;
     }
     return throwError(errorMessage);
   }
-
 }
