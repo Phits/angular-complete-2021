7d86caf0512afd813e323d4288ce09af8147f633
John Fitzpatrick <john.fitzpatrick@sendflex.com>
2021-04-01 17:24:23 -0400

    Working on reactive forms

---
+++ forms-reactive/src/app/app.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormArray, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  genders = ['Male', 'Female'];
+  signUpForm: FormGroup;
+  forbiddenUsernames = ['Chris', 'Anne'];
+
+
+  ngOnInit() {
+    this.signUpForm = new FormGroup({
+      'userData': new FormGroup({
+        'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
+        'email': new FormControl(null, [Validators.required, Validators.email]),
+      }),
+      'gender': new FormControl('Male'),
+      'hobbies': new FormArray([])
+    });
+  }
+
+  onAddHobby() {
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.signUpForm.get('hobbies')).push(control);
+  }
+
+  get controls() {
+    return (this.signUpForm.get('hobbies') as FormArray).controls;
+  }
+
+  forbiddenNames(control: FormControl): {[s: string]: boolean} {
+    if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
+      return {'nameIsForbidden': true};
+    } else {
+      return null;
+    }
+  }
+
+  onSubmit() {
+    console.log(this.signUpForm);
+  }
+
+}
